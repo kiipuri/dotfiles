0d4a6effb8aad95dd96458372cb31aa7e18f4774
kiipuri <kiipuri01@gmail.com>
2022-06-24 08:00:28 +0300

    lightdm in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# make, fakeroot
+
 cd dwm && sudo make install
 cd ../dmenu && sudo make install
 cd ..
@@ -25,3 +27,12 @@ rm -rf yay-git
 yay -S --noconfirm - < packages.txt
 
 chsh -s /bin/zsh
+
+sudo systemctl enable lightdm
+
+# Set default lightdm greeter to lightdm-webkit2-greeter
+sudo sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
+
+# Set default lightdm-webkit2-greeter theme to Glorious
+sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = glorious #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo sed -i 's/^debug_mode\s*=\s*\(.*\)/debug_mode = true #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf

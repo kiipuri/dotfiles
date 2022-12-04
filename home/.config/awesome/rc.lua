abf231e2231ec9dcf369ecb54ec23013a971345a
kiipuri <kiipuri01@gmail.com>
2022-12-04 12:25:12 +0200

    Remove quit hotkey from awesome

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -348,8 +348,8 @@ globalkeys = gears.table.join(
         { description = "open a terminal", group = "launcher" }),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
         { description = "reload awesome", group = "awesome" }),
-    awful.key({ modkey, "Shift" }, "q", awesome.quit,
-        { description = "quit awesome", group = "awesome" }),
+    -- awful.key({ modkey, "Shift" }, "q", awesome.quit,
+    --     { description = "quit awesome", group = "awesome" }),
 
     awful.key({ modkey, }, "l", function() awful.tag.incmwfact(0.05) end,
         { description = "increase master width factor", group = "layout" }),
@@ -561,7 +561,7 @@ awful.rules.rules = {
             "Gpick",
             "Kruler",
             "MessageWin", -- kalarm.
-            "Sxiv",
+            -- "Sxiv",
             "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
             "Wpa_gui",
             "veromix",

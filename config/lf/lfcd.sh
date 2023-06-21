6af2353c297e67a1996d564e514878834ab50d79
kiipuri <kiipuri@proton.me>
2023-06-21 14:37:21 +0300

    lf: switch from ueberzug to kitty icat

--- config/lf/lfcd.sh
+++ config/lf/lfcd.sh
@@ -1,16 +1,5 @@
-cleanup() {
-    exec 3>&-
-	rm "$FIFO_UEBERZUG"
-}
-
 lf () {
     set +m
-	[ ! -d "$HOME/.cache/lf" ] && mkdir --parents "$HOME/.cache/lf"
-	export FIFO_UEBERZUG="$HOME/.cache/lf/ueberzug-$$"
-	mkfifo "$FIFO_UEBERZUG"
-	ueberzug layer -s <"$FIFO_UEBERZUG" -p json &
-	exec 3>"$FIFO_UEBERZUG"
-	trap cleanup EXIT
 
     tmp="$(mktemp)"
     /usr/bin/lf --last-dir-path="$tmp" "$@"
